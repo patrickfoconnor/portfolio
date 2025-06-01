48c8943c9c4bf334f72b748c6cfd696e5a89785f
Patrick O'Connor <patrickoconnoricloud@gmail.com>
2025-06-01 00:21:37 -0500

    Fix repo path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/patrickfoconnor-website/",
+  base: "/portfolio/",
   plugins: [react()],
 });
